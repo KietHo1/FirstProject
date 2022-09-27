45ec6d300e00128bf1e3b9ee581ea8fcca94e25b
Kiet Ho <kiethostartup1999@gmail.com>
2022-09-27 16:55:01 +0700

    Sua lai

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          KietHo da lam git thanh cong!!!
-        </p>
-        <p>Day la noi dung bai hoc:</p>
-        <p>
-          <p>Đưa React Project lên Github:</p>
-          <p>git remote add origin git@github.com:kiethostartup/FirstProject.git</p>
-          <p>git push -u origin master</p>
-        </p>
-      </header>
+    <div className="Header">
+      <div>Hello</div>
     </div>
   );
 }
