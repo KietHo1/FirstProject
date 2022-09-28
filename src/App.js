551e8ac3a6255347c2b1892f4a61c8469feba771
Kiet Ho <kiethostartup1999@gmail.com>
2022-09-28 16:18:42 +0700

    Finish day 2

--- src/App.js
+++ src/App.js
@@ -15,9 +15,18 @@ function App() {
         <p className = "SDT">Số điện thoại:</p>
         <input type="text" className="telenumber" /><br></br>
       </form>
-      <img src = "https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/JoJo_Part_1_Phantom_Blood.jpg/250px-JoJo_Part_1_Phantom_Blood.jpg" 
-      className="images1" alt="images1"></img>
-      <img src = "https://upload.wikimedia.org/wikipedia/vi/d/d4/Jojo_vol_11.jpg" className="images2" alt="images2"></img>
+      <div className="phan1">
+        <div className="phancon1">
+          <img src = "https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/JoJo_Part_1_Phantom_Blood.jpg/250px-JoJo_Part_1_Phantom_Blood.jpg" 
+          className="images1" alt="images1"></img>
+          <p className = "p1">JoJo phần 1</p>
+        </div>
+        <div className="phancon2">
+          <img src = "https://upload.wikimedia.org/wikipedia/vi/d/d4/Jojo_vol_11.jpg" 
+          className="images2" alt="images2"></img>
+          <p className="p2">JoJo phần 2</p>
+        </div>
+      </div>
       <img src='https://cdn.vinshine.com/img/60f7bf1d0bc99426cac2388fe1b6e416442e04059ba626d5cd2507f27a4dfc7788bec6631afb6a74ff77a52a1336179312e87ba794ac5cf5.jpg?referer=60f7bf1d0bc994266d573f4b63d84278f6930556344116c3' 
       className = 'id0' alt='images'></img>
         <p className = 'id1'>
