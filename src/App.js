1eb5d1de8b6d41f6021ca7eedcde26c80e1ef40e
Kiet Ho <kiethostartup1999@gmail.com>
2022-09-29 15:23:09 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
       <div className="TieuDe">
         <img src="https://truyencc.com/images/logo.png" className="imagesheader" alt="imagesheader"></img>
-        <p>Trang Chủ</p>
-        <p>About</p>
+        <p className="TieuDe1">Trang Chủ</p>
+        <p className="TieuDe2">About</p>
       </div>
       <img src={logo} className="App-logo" alt="logo"/>
       <p className="Hello">Chào mừng bạn đến với website truyện JOJO của Kiệt Hồ</p>
@@ -25,14 +25,14 @@ function App() {
         <div className="phancon1">
           <img src = "https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/JoJo_Part_1_Phantom_Blood.jpg/250px-JoJo_Part_1_Phantom_Blood.jpg" 
           className="images1" alt="images1"></img>
-          <a href="https://jojo.fandom.com/wiki/Phantom_Blood">
+          <a href="https://jojo.fandom.com/wiki/Phantom_Blood" className = "a1">
             <p className = "p1">JoJo phần 1</p>
           </a>
         </div>
         <div className="phancon2">
           <img src = "https://upload.wikimedia.org/wikipedia/vi/d/d4/Jojo_vol_11.jpg" 
           className="images2" alt="images2"></img>
-          <a href="https://jojo.fandom.com/wiki/Battle_Tendency">
+          <a href="https://jojo.fandom.com/wiki/Battle_Tendency" className = "a2">
             <p className="p2">JoJo phần 2</p>
           </a>
         </div>
@@ -41,15 +41,15 @@ function App() {
         <div className="phancon3">
           <img src = "https://upload.wikimedia.org/wikipedia/en/7/75/JoJo_Part_3_Stardust_Crusaders.jpg" 
           className="images3" alt="images3"></img>
-          <a href="https://jojo.fandom.com/wiki/Stardust_Crusaders">
-            <p className = "p1">JoJo phần 3</p>
+          <a href="https://jojo.fandom.com/wiki/Stardust_Crusaders" className = "a3">
+            <p className = "p3">JoJo phần 3</p>
           </a>
         </div>
         <div className="phancon4">
           <img src = "https://upload.wikimedia.org/wikipedia/en/8/87/Part_4_Final_Poster.png" 
           className="images4" alt="images4"></img>
-          <a href="https://jojo.fandom.com/wiki/Diamond_is_Unbreakable">
-            <p className="p2">JoJo phần 4</p>
+          <a href="https://jojo.fandom.com/wiki/Diamond_is_Unbreakable" className = "a4">
+            <p className="p4">JoJo phần 4</p>
           </a>
         </div>
       </div>
@@ -58,15 +58,15 @@ function App() {
         <div className="phancon5">
           <img src = "https://upload.wikimedia.org/wikipedia/en/6/66/JoJo_Part_5_Golden_Wind.jpg" 
           className="images5" alt="images5"></img>
-          <a href="https://jojo.fandom.com/wiki/Vento_Aureo">
-            <p className = "p1">JoJo phần 5</p>
+          <a href="https://jojo.fandom.com/wiki/Vento_Aureo" className = "a5">
+            <p className = "p5">JoJo phần 5</p>
           </a>
         </div>
         <div className="phancon6">
           <img src = "http://pm1.narvii.com/5883/b4178b2fdba43057f662b27dd22c4ad535cb1d8c_00.jpg" 
           className="images6" alt="images6"></img>
-          <a href="https://jojo.fandom.com/wiki/Stone_Ocean">
-            <p className="p2">JoJo phần 6</p>
+          <a href="https://jojo.fandom.com/wiki/Stone_Ocean" className = "a6">
+            <p className="p6">JoJo phần 6</p>
           </a>
         </div>
       </div>
@@ -75,15 +75,15 @@ function App() {
         <div className="phancon7">
           <img src = "http://st.cdjapan.co.jp/pictures/l/14/35/NEOBK-2173350.jpg" 
           className="images7" alt="images7"></img>
-          <a href="https://jojo.fandom.com/wiki/Steel_Ball_Run">
-             <p className = "p1">JoJo phần 7</p>
+          <a href="https://jojo.fandom.com/wiki/Steel_Ball_Run" className = "a7">
+             <p className = "p7">JoJo phần 7</p>
           </a>
         </div>
         <div className="phancon8">
           <img src = "https://i.pinimg.com/originals/28/5e/bd/285ebd327749cdbed68ea573db8210fe.jpg" 
           className="images8" alt="images8"></img>
-          <a href="https://jojo.fandom.com/wiki/JoJolion">
-            <p className="p2">JoJo phần 8</p>
+          <a href="https://jojo.fandom.com/wiki/JoJolion" className = "a8">
+            <p className="p8">JoJo phần 8</p>
           </a>
 
         </div>
